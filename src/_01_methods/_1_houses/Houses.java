3267d51a10d41b7576b01dac85d25d11a7364a55
zhaoyd5268 <88736478+zhaoyd5268@users.noreply.github.com>
2022-01-20 16:51:35 -0800

    houses

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,4 +1,9 @@
 package _01_methods._1_houses;
+
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
@@ -7,5 +12,23 @@ public class Houses {
 		
 	public void run() {
 		// Check the recipe to find out what code to put here
+	Robot rob = new Robot();
+	rob.setX(20);
+	rob.setSpeed(20);
+	for (int i = 0; i<9; i++) {
+	rob.setPenColor(Color.orange);
+	rob.penDown();
+	rob.move(100);
+	rob.turn(90);
+	rob.move(20);
+	rob.turn(90);
+	rob.move(100);
+	rob.setPenColor(Color.GREEN);
+	rob.turn(-90);
+	rob.move(30);
+	rob.turn(-90);
+
+	}
+	rob.hide();
 	}
 }
